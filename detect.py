d5020756e1917462cfdbbf3b2528c29dab8bc1b6
crispychicken20 <cperez29@student.ohlone.edu>
2025-09-30 08:45:53 -0700

    Added skeleton tasks

---
+++ detect.py
@@ -0,0 +1,9 @@
+# WIll be utlizing a YOLO model or a vision model for object detection
+
+# TASK 1: Install ultralytics + text YOLOv8 or LLaVa on sample webcam frame
+# TASK 2: Write wraper detect_objects(frame) -> returns labels
+# TASK 3: Integrate with OpenCV webcam capture loop
+# TASK 4: Add trigger: "TARS, what do you see?" -> runs model and answers
+# TASK 5: Log detected objects for debugging.
+# TASK 6: API call to GPT that can be to sent to frontend to tell user
+	# what object is being detected
